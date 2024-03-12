0effaf9d3b2e4db08f6a2ce992bfa1aeff6a3ad3
Mobbbb <mobbbb.top@gmail.com>
2024-03-12 11:17:11 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+"use strict"
+
+var express             = require('express');
+var app                 = express();
+var bodyParse           = require('body-parser');
+var cookieParser        = require('cookie-parser');
+var request             = require('./request');
+
+app.use(cookieParser());
+app.use(bodyParse.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With, authKey, sessionid');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+    if (req.method == 'OPTIONS') {
+        res.send('200');
+    } else {
+        next();
+    }
+});
+
+request(app);
+
+app.listen(3000, function () {
+    console.log('   ');console.log('   ');
+    console.log('   App listening at:');
+	console.log('   - Network: http://127.0.0.1:3000');
+    console.log('   ');console.log('   ');
+});
